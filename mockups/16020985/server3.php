897ba1e363e230301c1ab4ebd370be5a789685a2
HoangHung <hvhung98@gmail.com>
2018-11-19 01:46:26 +0700

    aa

--- mockups/16020985/server3.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include('server.php');
-    $idbaidang = $_SESSION['idbaidang'];
-    $sql_count_like = "SELECT yeuthich FROM baidang WHERE idbaidang='$idbaidang'";
-    $result = mysqli_query($db, $sql_count_like);
-    echo mysqli_fetch_array($result)['yeuthich'];
-?>
