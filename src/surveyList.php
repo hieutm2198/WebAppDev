9976947e28c6bd00de719dc8d4ccea8195bd1f1c
Tran Minh Hieu <iamacomplexperson@gmail.com>
2018-11-25 19:57:39 +0700

    cap nhat

---
+++ src/surveyList.php
@@ -0,0 +1,18 @@
+<?php
+    include("database.php");
+    $sql = "select * from khaosat join cuusinhvien on khaosat.MaNguoiTao = cuusinhvien.idcuusinhvien where khaosat.TrangThaiKS = 'published';";
+    $result = $connection -> query($sql);
+    
+    $response;
+
+    while($row = $result -> fetch_assoc()) {
+        $response -> data[] = array(
+            surveyId => $row['MaKS'],
+            surveyTitle => $row['TieuDe'],
+            creatorId => $row['MaNguoiTao'],
+            creatorName => $row['hoten']
+            
+        );
+    }
+    die(json_encode($response, JSON_UNESCAPED_UNICODE));
+?>
